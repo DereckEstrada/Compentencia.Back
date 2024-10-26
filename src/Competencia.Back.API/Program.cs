1e33b6a4ca1819a7564c8544ca86e73d173db460
Dereck Estrada Molina <dereckdem17@gmail.com>
2024-10-26 12:05:27 -0500

    Cors

--- src/Competencia.Back.API/Program.cs
+++ src/Competencia.Back.API/Program.cs
@@ -6,6 +6,17 @@ using Competencia.Back.Entities.Entities;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+         "MyAllowSpecificOrigins",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                              .AllowAnyHeader()
+                              .AllowAnyMethod();
+        });
+});
 
 // Add services to the container.
 
@@ -18,6 +29,7 @@ builder.Services.AddScoped<IPersonaServices, PersonaServices>();
 builder.Services.AddScoped<IOficinaServices, OficinaServices>();
 builder.Services.AddScoped<IReservaServices, ReservaServices>();
 var app = builder.Build();
+app.UseCors("MyAllowSpecificOrigins");
 
 // Configure the HTTP request pipeline.
 
